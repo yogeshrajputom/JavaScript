f18a3bf31095f98574a587236425ca9eb59ed56c
Yogesh Rajput <153723012+yogeshrajputom@users.noreply.github.com>
2025-10-23 10:18:51 +0000

    simple conversion

---
+++ basics/conversion.js
@@ -0,0 +1,28 @@
+const score = "33";
+const newScore = "485sfdsa";
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let inNumber = Number (score);
+//console.log(typeof inNumber);
+
+let inNumbers = Number (newScore);
+//console.log(typeof inNumbers);
+//console.log(inNumbers);             //NaN
+
+// "33" => 33
+// "33fd" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "jghh";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "yoge" => true

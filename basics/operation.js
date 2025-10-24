93645162fc35b4871363e3fad1807490ece31cdf
Yogesh Rajput <153723012+yogeshrajputom@users.noreply.github.com>
2025-10-24 06:12:48 +0000

    simple operation

---
+++ basics/operation.js
@@ -0,0 +1,16 @@
+let value = 3;
+let values = -value;
+console.log(values);
+
+
+// console.log(4 + 3);         //7
+// console.log(4 - 3);         //1 
+// console.log(4 * 3);         //12
+// console.log(4 / 3);         //1.3
+// console.log(4 % 3);         //1
+// console.log(4 ** 3);        //64
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 +2);

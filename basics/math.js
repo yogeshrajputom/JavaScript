dad88340aacd88433269529b5cc13f156a4c93a4
Yogesh Rajput <153723012+yogeshrajputom@users.noreply.github.com>
2025-10-24 15:01:13 +0000

    Math conseps

---
+++ basics/math.js
@@ -0,0 +1,25 @@
+let score = 400;
+let valance = new Number(450);
+// console.log(valance);
+// 
+// console.log(valance.toString().length);
+// console.log(valance.toFixed(2));
+// console.log(valance.toFixed(1));
+
+
+const otherNumber = 123.254;
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//======================= Mathe ==================
+
+console.log(Math.abs(-5));
+console.log(Math.round(4.4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(5.2));
+console.log(Math.floor(5.6));
+

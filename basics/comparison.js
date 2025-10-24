40b79b5f639dd39cc2c46a99c0dbbab659b3c034
Yogesh Rajput <153723012+yogeshrajputom@users.noreply.github.com>
2025-10-24 07:16:50 +0000

    commit

---
+++ basics/comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//  console.log(null > 0);           // avoid this type of conversion
+//  console.log(null >= 0);          // avoid this type of conversion
+//  console.log(null === 0);             // avoid this type of conversion
+//  console.log(null < 0);           // avoid this type of conversion
+
+//  console.log(undefined > 0);          // avoid this type of conversion
+//  console.log(undefined == 0);             // avoid this type of conversion
+//  console.log(undefined < 0);                      // avoid this type of conversion
+
+ // ===   strict conversion
+
+ console.log(2 === "2");

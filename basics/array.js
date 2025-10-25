accdfb7625859a00e36efd73324519e83734b472
Yogesh Rajput <153723012+yogeshrajputom@users.noreply.github.com>
2025-10-25 09:36:56 +0000

    array

---
+++ basics/array.js
@@ -0,0 +1,33 @@
+let array = [1,2,3,4,5,6];
+
+//array.push(10);
+//array.pop();
+
+// array.unshift(9);
+// console.log(array);
+
+// array.shift();
+// console.log(array);
+
+// console.log(array.indexOf(9));
+// console.log(array.indexOf(5));
+
+let newArray = array.join();
+// console.log(array);
+// console.log(newArray);
+// console.log(typeof newArray);
+
+
+
+// Slice and splice 
+
+let arrN = array.slice(1, 4);
+console.log(arrN);
+console.log("A",array);
+
+let arrN1 = array.splice(1, 4);
+console.log(arrN1);
+console.log("B", array)
+
+
+

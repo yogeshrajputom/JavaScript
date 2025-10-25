0fb298476e12411880e4a4468ff79c98f5376f2c
Yogesh Rajput <153723012+yogeshrajputom@users.noreply.github.com>
2025-10-25 08:13:45 +0000

    date

---
+++ basics/data.js
@@ -0,0 +1,19 @@
+let myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString);
+// console.log(myDate.toLocaleDateString);
+// console.log(myDate.toTimeString);
+
+
+let myCreateDate = new Date(1998 , 9, 20);
+// console.log(myCreateDate.toDateString());
+
+let myTimeStemp = Date.now();
+console.log(myTimeStemp);
+console.log(myCreateDate.getTime());
+

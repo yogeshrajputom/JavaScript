6a71d24783407aba432728e0855406052c2b2766
Yogesh Rajput <153723012+yogeshrajputom@users.noreply.github.com>
2025-10-25 14:31:24 +0000

    object

---
+++ basics2/object3.js
@@ -0,0 +1,13 @@
+const course = {
+    name:"java DSA",
+    price:495,
+    instructor:"Harry"
+}
+
+// console.log(course.instructor)
+
+//const {instructor} = course;
+//console.log(instructor);
+
+const {instructor: tacher} = course; 
+console.log(tacher)

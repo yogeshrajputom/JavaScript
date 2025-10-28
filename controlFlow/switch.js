511ed189f071c000676eb4f5b7aae49dbcec127c
Yogesh Rajput <153723012+yogeshrajputom@users.noreply.github.com>
2025-10-28 08:37:08 +0000

    switch

---
+++ controlFlow/switch.js
@@ -0,0 +1,36 @@
+/*switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}*/
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("feb");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
+    case 5:
+        console.log("may");
+        
+        break;
+
+    default:
+        console.log("This is not a Monthe");
+        
+        break;
+}

31b7f47601f44303c66631b9ab4e9726b9159748
Yogesh Rajput <153723012+yogeshrajputom@users.noreply.github.com>
2025-10-28 06:17:01 +0000

    scope

---
+++ basics2/scope.js
@@ -0,0 +1,16 @@
+// Advance 
+
+console.log(one(5));            //this is not show any error
+
+function one (num){
+    return num +1;
+}
+
+
+
+// console.log(two(8));         This is show error 
+
+const two = function(num) {
+    return num+2;
+}
+

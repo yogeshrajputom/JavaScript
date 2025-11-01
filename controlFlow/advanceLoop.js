cd18686ceda64137806a4899638090c3ad86db5c
Yogesh Rajput <153723012+yogeshrajputom@users.noreply.github.com>
2025-11-01 22:14:37 +0530

    Implement array filtering examples in advanceLoop.js
    Added filtering examples using arrays and objects.

--- controlFlow/advanceLoop.js
+++ controlFlow/advanceLoop.js
@@ -8,3 +8,55 @@
 const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const newNum = myNum.filter((num) => num > 5)
 console.log(newNum);
+// const Num = myNum.filter((item) => {
+//     return item > 8;
+// })
+// console.log(Num);
+
+// same work useing for each loops
+
+const num = [];
+myNum.forEach((items) => {
+    if (items > 4) {
+        num.push(items);
+    }
+})
+// console.log(num);
+
+const books =[
+    {
+        titel:"Bhart ek Khoj",
+        genre:"History",
+        publish:1935,
+        edition:2022
+    },
+       {
+        titel:"Shaty ke shath mera priyog",
+        genre:"History",
+        publish:1946,
+        edition:2024
+    },
+       {
+        titel:"me nastik kiyo",
+        genre:"Biography",
+        publish:1985,
+        edition:2018
+    },
+       {
+        titel:"Gitanjli",
+        genre:"poitery",
+        publish:1900,
+        edition:2023
+    },
+       {
+        titel:"Gita",
+        genre:"Spritual",
+        publish:1855,
+        edition:2012
+    }
+]
+
+const mybk = books.filter((bk) => bk.genre === "History")
+// console.log(mybk)
+mybk.filter((bk)=> bk.publish >= 1930)
+console.log(mybk)
